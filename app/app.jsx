df908d0715ea32092d1bea1b57984a5707e359e7
Michael Fahey <michael@perception.ie>
2018-03-28 20:25:24 +0100

    Switch to scss

--- app/app.jsx
+++ app/app.jsx
@@ -12,7 +12,7 @@ $(document).foundation();
 
 // styles/app.css
 
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
     <Router history={hashHistory}>
